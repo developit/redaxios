595e304c5de7ff3f26f86cc7f6267d2d13d7162d
Jason Miller <developit@users.noreply.github.com>
2020-07-23 02:06:03 +0000

    Update tests

--- test/index.test.js
+++ test/index.test.js
@@ -11,39 +11,43 @@
  * limitations under the License.
  */
 
-// import sinon from 'sinon';
-import axios from '../src/index.js';
+// @ts-ignore
 import textExample from 'file-loader!./fixtures/example.txt';
+// @ts-ignore
 import jsonExample from 'file-loader!./fixtures/example.json.txt';
+import axios from '../src/index.js';
 import fetch from 'isomorphic-fetch';
 
 describe('redaxios', () => {
-	it('smoke test', () => {
-		expect(axios).toBeInstanceOf(Function);
+	describe('basic functionality', () => {
+		it('should return text and a 200 status for a simple GET request', async () => {
+			const req = axios(textExample);
+			expect(req).toBeInstanceOf(Promise);
+			const res = await req;
+			expect(res).toBeInstanceOf(Object);
+			expect(res.status).toEqual(200);
+			expect(res.data).toEqual('some example content');
+		});
+
+		it('should return a rejected promise for 404 responses', async () => {
+			const req = axios('/foo.txt');
+			expect(req).toBeInstanceOf(Promise);
+			const spy = jasmine.createSpy();
+			await req.catch(spy);
+			expect(spy).toHaveBeenCalledTimes(1);
+			expect(spy).toHaveBeenCalledWith(jasmine.objectContaining({ status: 404 }));
+		});
 	});
 
-	it('should reject promises for 404', async () => {
-		const req = axios('/foo.txt');
-		expect(req).toBeInstanceOf(Promise);
-		const spy = jasmine.createSpy();
-		await req.catch(spy);
-		expect(spy).toHaveBeenCalledTimes(1);
-		expect(spy).toHaveBeenCalledWith(jasmine.objectContaining({ status: 404 }));
-	});
-
-	it('should request a file', async () => {
-		const req = axios(textExample);
-		expect(req).toBeInstanceOf(Promise);
-		const res = await req;
-		expect(res).toBeInstanceOf(Object);
-		expect(res.status).toEqual(200);
-		expect(res.data).toEqual('some example content');
-	});
-
-	describe('responseType', () => {
-		it('should default to JSON', async () => {
+	describe('options.responseType', () => {
+		it('should parse responses as JSON by default', async () => {
 			const res = await axios.get(jsonExample);
 			expect(res.data).toEqual({ hello: 'world' });
+		});
+
+		it('should fall back to text for non-JSON by default', async () => {
+			const res = await axios.get(textExample);
+			expect(res.data).toEqual('some example content');
 		});
 
 		it('should force JSON for responseType:json', async () => {
@@ -53,7 +57,7 @@ describe('redaxios', () => {
 			expect(res.data).toEqual({ hello: 'world' });
 		});
 
-		it('should fall back to null for failed JSON parse', async () => {
+		it('should fall back to undefined for failed JSON parse', async () => {
 			const res = await axios.get(textExample, {
 				responseType: 'json'
 			});
@@ -69,108 +73,109 @@ describe('redaxios', () => {
 		});
 	});
 
-	it('response should be parsed json', async () => {
-		const req = axios.get(jsonExample);
-		expect(req).toBeInstanceOf(Promise);
-		const res = await req;
-		expect(res).toBeInstanceOf(Object);
-		expect(res.status).toEqual(200);
-		expect(res.data).toBeInstanceOf(Object);
+	describe('options.baseURL', () => {
+		it('should resolve URLs relative to baseURL if provided', async () => {
+			const oldFetch = window.fetch;
+			try {
+				window.fetch = jasmine
+					.createSpy('fetch')
+					.and.returnValue(Promise.resolve({ ok: true, status: 200, text: () => Promise.resolve('') }));
+				const req = axios.get('/bar', {
+					baseURL: 'http://foo'
+				});
+				expect(window.fetch).toHaveBeenCalledTimes(1);
+				expect(window.fetch).toHaveBeenCalledWith(
+					'http://foo/bar',
+					jasmine.objectContaining({
+						method: 'get',
+						headers: {},
+						body: undefined
+					})
+				);
+				const res = await req;
+				expect(res.status).toEqual(200);
+			} finally {
+				window.fetch = oldFetch;
+			}
+		});
 	});
 
-	it('should make a request with a baseURL', async () => {
-		const oldFetch = window.fetch;
-		try {
-			window.fetch = jasmine
-				.createSpy('fetch')
-				.and.returnValue(Promise.resolve({ ok: true, status: 200, text: () => Promise.resolve('') }));
-			const req = axios.get('/bar', {
-				baseURL: 'http://foo'
-			});
-			expect(window.fetch).toHaveBeenCalledTimes(1);
-			expect(window.fetch).toHaveBeenCalledWith(
-				'http://foo/bar',
-				jasmine.objectContaining({
-					method: 'get',
-					headers: {},
-					body: undefined
-				})
-			);
-			const res = await req;
-			expect(res.status).toEqual(200);
-		} finally {
-			window.fetch = oldFetch;
-		}
-	});
+	describe('options.headers', () => {
+		it('should merge headers case-insensitively', async () => {
+			const oldFetch = window.fetch;
+			try {
+				const fetch = (window.fetch = jasmine.createSpy('fetch').and.returnValue(
+					Promise.resolve({
+						ok: true,
+						status: 200,
+						text: () => Promise.resolve('yep')
+					})
+				));
+				await axios('/', { headers: { 'x-foo': '2' } });
+				expect(fetch.calls.first().args[1].headers).toEqual({
+					'x-foo': '2'
+				});
 
-	it('should merge headers case-insensitively', async () => {
-		const oldFetch = window.fetch;
-		try {
-			const fetch = (window.fetch = jasmine.createSpy('fetch').and.returnValue(
-				Promise.resolve({
-					ok: true,
-					status: 200,
-					text: () => Promise.resolve('yep')
-				})
-			));
-			await axios('/', { headers: { 'x-foo': '2' } });
-			expect(fetch.calls.first().args[1].headers).toEqual({
-				'x-foo': '2'
-			});
+				fetch.calls.reset();
 
-			fetch.calls.reset();
+				await axios('/', { headers: { 'x-foo': '2', 'X-Foo': '4' } });
+				expect(fetch.calls.first().args[1].headers).toEqual({
+					'x-foo': '4'
+				});
 
-			await axios('/', { headers: { 'x-foo': '2', 'X-Foo': '4' } });
-			expect(fetch.calls.first().args[1].headers).toEqual({
-				'x-foo': '4'
-			});
+				fetch.calls.reset();
 
-			fetch.calls.reset();
-
-			const request = axios.create({
-				headers: {
-					'Base-Upper': 'base',
+				const request = axios.create({
+					headers: {
+						'Base-Upper': 'base',
+						'base-lower': 'base'
+					}
+				});
+				await request('/');
+				expect(fetch.calls.first().args[1].headers).toEqual({
+					'base-upper': 'base',
 					'base-lower': 'base'
-				}
-			});
-			await request('/');
-			expect(fetch.calls.first().args[1].headers).toEqual({
-				'base-upper': 'base',
-				'base-lower': 'base'
-			});
+				});
 
-			fetch.calls.reset();
+				fetch.calls.reset();
 
-			await request('/', {
-				headers: {
+				await request('/', {
+					headers: {
+						'base-upper': 'replaced',
+						'BASE-LOWER': 'replaced'
+					}
+				});
+				expect(fetch.calls.first().args[1].headers).toEqual({
 					'base-upper': 'replaced',
-					'BASE-LOWER': 'replaced'
-				}
-			});
-			expect(fetch.calls.first().args[1].headers).toEqual({
-				'base-upper': 'replaced',
-				'base-lower': 'replaced'
-			});
-		} finally {
-			window.fetch = oldFetch;
-		}
+					'base-lower': 'replaced'
+				});
+			} finally {
+				window.fetch = oldFetch;
+			}
+		});
 	});
 
-	it('should issue POST requests', async () => {
-		const oldFetch = window.fetch;
-		try {
-			window.fetch = jasmine.createSpy('fetch').and.returnValue(
+	describe('options.body (request bodies)', () => {
+		let oldFetch, fetchMock;
+		beforeEach(() => {
+			oldFetch = window.fetch;
+			fetchMock = window.fetch = jasmine.createSpy('fetch').and.returnValue(
 				Promise.resolve({
 					ok: true,
 					status: 200,
 					text: () => Promise.resolve('yep')
 				})
 			);
-			const req = axios.post('/foo', {
+		});
+		afterEach(() => {
+			window.fetch = oldFetch;
+		});
+
+		it('should issue POST requests (with JSON body)', async () => {
+			const res = await axios.post('/foo', {
 				hello: 'world'
 			});
-			expect(window.fetch).toHaveBeenCalledTimes(1);
-			expect(window.fetch).toHaveBeenCalledWith(
+			expect(fetchMock).toHaveBeenCalledWith(
 				'/foo',
 				jasmine.objectContaining({
 					method: 'post',
@@ -180,63 +185,42 @@ describe('redaxios', () => {
 					body: '{"hello":"world"}'
 				})
 			);
-			const res = await req;
 			expect(res.status).toEqual(200);
 			expect(res.data).toEqual('yep');
-		} finally {
-			window.fetch = oldFetch;
-		}
-	});
+		});
 
-	it('should not send JSON content-type when data contains FormData', async () => {
-		const oldFetch = window.fetch;
-		try {
-			window.fetch = jasmine.createSpy('fetch').and.returnValue(Promise.resolve());
-			axios.post('/foo', new FormData());
-			expect(window.fetch).toHaveBeenCalledTimes(1);
-			expect(window.fetch).not.toHaveBeenCalledWith(
-				'/foo',
-				jasmine.objectContaining({
-					headers: {
-						'content-type': 'application/json'
-					}
-				})
-			);
-		} finally {
-			window.fetch = oldFetch;
-		}
-	});
+		describe('FormData support', () => {
+			it('should not send JSON content-type when data contains FormData', async () => {
+				const formData = new FormData();
+				await axios.post('/foo', formData);
+				expect(fetchMock).toHaveBeenCalledWith(
+					'/foo',
+					jasmine.objectContaining({
+						body: formData,
+						headers: {}
+					})
+				);
+			});
 
-	it('should preserve global content-type option when using FormData', async () => {
-		const oldFetch = window.fetch;
-		try {
-			window.fetch = jasmine.createSpy('fetch').and.returnValue(
-				Promise.resolve({
-					ok: true,
-					status: 200,
-					text: () => Promise.resolve('yep')
-				})
-			);
-			const data = new FormData();
-			data.append('hello', 'world');
-			const req = axios.post('/foo', data, { headers: { 'content-type': 'multipart/form-data' } });
-			expect(window.fetch).toHaveBeenCalledTimes(1);
-			expect(window.fetch).toHaveBeenCalledWith(
-				'/foo',
-				jasmine.objectContaining({
-					method: 'post',
-					headers: {
-						'content-type': 'multipart/form-data'
-					},
-					body: data
-				})
-			);
-			const res = await req;
-			expect(res.status).toEqual(200);
-			expect(res.data).toEqual('yep');
-		} finally {
-			window.fetch = oldFetch;
-		}
+			it('should preserve global content-type option when using FormData', async () => {
+				const data = new FormData();
+				data.append('hello', 'world');
+				const res = await axios.post('/foo', data, { headers: { 'content-type': 'multipart/form-data' } });
+				expect(fetchMock).toHaveBeenCalledTimes(1);
+				expect(fetchMock).toHaveBeenCalledWith(
+					'/foo',
+					jasmine.objectContaining({
+						method: 'post',
+						headers: {
+							'content-type': 'multipart/form-data'
+						},
+						body: data
+					})
+				);
+				expect(res.status).toEqual(200);
+				expect(res.data).toEqual('yep');
+			});
+		});
 	});
 
 	it('should accept a custom fetch implementation', async () => {
@@ -248,35 +232,45 @@ describe('redaxios', () => {
 		expect(res.data).toEqual({ hello: 'world' });
 	});
 
-	it('should support params and paramsSerializer options', async () => {
-		const oldFetch = window.fetch;
-		window.fetch = jasmine.createSpy('fetch').and.returnValue(Promise.resolve());
+	describe('options.params & options.paramsSerializer', () => {
+		let oldFetch, fetchMock;
+		beforeEach(() => {
+			oldFetch = window.fetch;
+			fetchMock = window.fetch = jasmine.createSpy('fetch').and.returnValue(Promise.resolve());
+		});
 
-		axios.get('/foo');
-		expect(window.fetch).toHaveBeenCalledTimes(1);
-		expect(window.fetch).toHaveBeenCalledWith('/foo', jasmine.any(Object));
+		afterEach(() => {
+			window.fetch = oldFetch;
+		});
 
-		let params;
+		it('should not serialize missing params', async () => {
+			axios.get('/foo');
+			expect(fetchMock).toHaveBeenCalledWith('/foo', jasmine.any(Object));
+		});
 
-		params = { a: 1, b: true };
-		axios.get('/foo', { params });
-		expect(window.fetch).toHaveBeenCalledTimes(2);
-		expect(window.fetch).toHaveBeenCalledWith('/foo?a=1&b=true', jasmine.any(Object));
+		it('should serialize numeric and boolean params', async () => {
+			const params = { a: 1, b: true };
+			axios.get('/foo', { params });
+			expect(fetchMock).toHaveBeenCalledWith('/foo?a=1&b=true', jasmine.any(Object));
+		});
 
-		axios.get('/foo?c=42', { params });
-		expect(window.fetch).toHaveBeenCalledTimes(3);
-		expect(window.fetch).toHaveBeenCalledWith('/foo?c=42&a=1&b=true', jasmine.any(Object));
+		it('should merge params into existing url querystring', async () => {
+			const params = { a: 1, b: true };
+			axios.get('/foo?c=42', { params });
+			expect(fetchMock).toHaveBeenCalledWith('/foo?c=42&a=1&b=true', jasmine.any(Object));
+		});
 
-		params = new URLSearchParams({ d: 'test' });
-		axios.get('/foo', { params });
-		expect(window.fetch).toHaveBeenCalledTimes(4);
-		expect(window.fetch).toHaveBeenCalledWith('/foo?d=test', jasmine.any(Object));
+		it('should accept a URLSearchParams instance', async () => {
+			const params = new URLSearchParams({ d: 'test' });
+			axios.get('/foo', { params });
+			expect(fetchMock).toHaveBeenCalledWith('/foo?d=test', jasmine.any(Object));
+		});
 
-		const paramsSerializer = (params) => 'e=iamthelaw';
-		axios.get('/foo', { params, paramsSerializer });
-		expect(window.fetch).toHaveBeenCalledTimes(5);
-		expect(window.fetch).toHaveBeenCalledWith('/foo?e=iamthelaw', jasmine.any(Object));
-
-		window.fetch = oldFetch;
+		it('should accept a custom paramsSerializer function', async () => {
+			const params = { a: 1, b: true };
+			const paramsSerializer = (params) => 'e=iamthelaw';
+			axios.get('/foo', { params, paramsSerializer });
+			expect(fetchMock).toHaveBeenCalledWith('/foo?e=iamthelaw', jasmine.any(Object));
+		});
 	});
 });
